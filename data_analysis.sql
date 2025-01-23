8ee991b313567a7d86efc7fc39301658f4b3f6ec
Nicolas Carballal <nicolas.carballal@externos-cl.cencosud.com>
2025-01-23 10:24:29 -0300

    fix table name

--- data_analysis.sql
+++ data_analysis.sql
@@ -1,18 +1,18 @@
 -- Análisis de Gastos
 SELECT categoria, format_datetime(fecha_transaccion, 'MMMM') mes, sum(gasto_credito)credito, sum(gasto_debito) debito
-FROM "default"."fakebank"
+FROM "default"."FakeBank_api"
 GROUP BY format_datetime(fecha_transaccion, 'MMMM'), categoria
 ORDER BY categoria
 
 -- Estado de cuentas mensual
 SELECT cliente, format_datetime(fecha_transaccion, 'MMMM') mes, sum(coalesce(gasto_credito, 0)) + sum(coalesce(gasto_debito, 0)) as saldo_total
-FROM "default"."fakebank"
+FROM "default"."FakeBank_api"
 GROUP BY cliente, month(fecha_transaccion), format_datetime(fecha_transaccion, 'MMMM')
 order by cliente, month(fecha_transaccion)
 
 
 SELECT cliente, sum(coalesce(gasto_credito, 0)) + sum(coalesce(gasto_debito, 0)) as saldo_total
-FROM "default"."fakebank"
+FROM "default"."FakeBank_api"
 GROUP BY cliente
 order by cliente
 
@@ -27,6 +27,6 @@ SELECT
         WHEN sum(coalesce(gasto_credito, 0)) + sum(coalesce(gasto_debito, 0)) >= 25 THEN 'Bajo'
         WHEN sum(coalesce(gasto_credito, 0)) + sum(coalesce(gasto_debito, 0)) > 0 THEN 'Muy bajo'
     END AS segmentacion
-FROM "default"."fakebank"
+FROM "default"."FakeBank_api"
 GROUP BY cliente, month(fecha_transaccion), format_datetime(fecha_transaccion, 'MMMM')
 order by cliente, month(fecha_transaccion)
